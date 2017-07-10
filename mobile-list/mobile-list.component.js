773531d3985da4fbb1b8101d2dc69285ff4d7f0f
StormRaider2495 <arunit95@gmail.com>
2017-07-10 11:43:33 +0530

    Updated Repo

--- mobile-list/mobile-list.component.js
+++ mobile-list/mobile-list.component.js
@@ -1,9 +1,8 @@
 // Register `mobileList` component, along with its associated controller and template
-
 angular.
   module('phonecatApp').
   component('mobileList', {
-  //  template: '<div ng-repeat="mobile in $ctrl.mobiles" > {{mobile.name}} </div>',
+
     templateUrl:'mobile-list/mobile-list.template.html',
     controller: ['$http','$scope',
      function MobileListController($http,$scope) {
@@ -17,6 +16,15 @@ angular.
          $scope.orderByMe = function(x) {
 
             $scope.myOrderBy = x;
+        }
+
+        $scope.gridView = function(){
+            angular.element('.mobile-list').addClass('grid-view');
+            angular.element('.snippet').css({'font-size':'16px'});
+        }
+        $scope.listView = function(){
+            angular.element('.mobile-list').removeClass('grid-view');
+            angular.element('.snippet').css({'font-size':'21px'});
         }
 
      }
